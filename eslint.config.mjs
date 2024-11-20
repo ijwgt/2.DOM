4569f7fc3b194ae1ccaf448b7c45f6b60636d383
ijwgt <66661966@bk.ru>
2024-11-20 22:24:50 +0300

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import jest from "eslint-plugin-jest";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   {
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   pluginJs.configs.recommended,
-  eslintPluginPrettierRecommended,
   {
     rules: {
       "no-unused-vars": "warn",
